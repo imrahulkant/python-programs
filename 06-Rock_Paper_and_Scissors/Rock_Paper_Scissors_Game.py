d2b3c5d2948d456bb1244d31c3694cb20669d80b
imrahulkant <rahulkant1000@gmail.com>
2020-10-06 16:10:47 +0530

    commit005

--- 06-Rock_Paper_and_Scissors/Rock_Paper_Scissors_Game.py
+++ 06-Rock_Paper_and_Scissors/Rock_Paper_Scissors_Game.py
@@ -1,13 +1,8 @@
-#START;
-
 import random
 
-#DEFAULT;
 my_dict={'R':"Rock",'P':"Paper",'S':"Scissors"}
 user_count=0
 comp_count=0
-
-#INPUT;
 
 games=int(input("\nEnter the number of games you want to play: "))
 
@@ -20,27 +15,21 @@ print(
 	)
 
 while(comp_count+user_count<games):
-	#WHILE LOOP STARTS;
-
 	flag=0
 
 	user_input=input("\nUser's Input: ")[0]
 	user_input=user_input.upper()
-	#The [0] after the input() will assign the first charcter of input to the variable;
-	#Hence, the user can enter anything, anyway; 
-	#Example: The user can enter Rock or rock or r or R or ro or any such thing which represents Rock;
-	#It will always take input as a R
-	#Thereby, increasing the user input window;
+	
 
 	for i in my_dict.keys():
-		if(user_input==i):		#If the entered input is confined to Rock, Paper or Scissors;
+		if(user_input==i):		
 			flag=1
 			break
-	if(flag!=1):				#If not, run the loop again;
+	if(flag!=1):				
 		print("INVALID INPUT")
 		continue
 
-	comp_input=random.choice(list(my_dict.keys()))	#Random Key from the dictionary my_dict i.e. R,P or S;
+	comp_input=random.choice(list(my_dict.keys()))	
 
 	print("Computer's Input: ", my_dict[comp_input])
 	if ( user_input=='R' and comp_input=='P' ) or ( user_input=='P' and comp_input=='S' ) or ( user_input=='S' and comp_input=='R' ):
@@ -53,7 +42,7 @@ while(comp_count+user_count<games):
 	print("\nSCORE:")
 	print("User Score:",user_count,"\tComputer Score:",comp_count,"\n")
 
-	#LOOP ENDS;
+	
 
 print("\n\t\tFINAL SCORE:")
 print("User Score:",user_count,"\t\t\tComputer Score:",comp_count,"\n")
@@ -64,6 +53,3 @@ elif user_count<comp_count:
 else:
 	print("\n\t\tOOPS! IT'S A TIE!")
 
-
-
-#END;
